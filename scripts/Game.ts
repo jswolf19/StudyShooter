a17125c9499e1cf135f86ad520e62f880402a9d7
jswolf19 <jswolf19@hotmail.com>
2021-06-05 21:01:23 +0900

    ゲームループ追加

--- scripts/Game.ts
+++ scripts/Game.ts
@@ -1,6 +1,7 @@
 class Game {
     private static readonly ZOOM_FACTOR: number = 2;
     private static readonly STAR_CNT = 300;
+    private static readonly GAME_SPEED = 1000 / 60;
 
     public readonly SCREEN_SIZE: Size;
 
@@ -12,6 +13,7 @@ class Game {
     }
     
     private readonly _ctx: CanvasRenderingContext2D;
+    private _loopHandle: number;
 
     private readonly _stars: Array<Star>;
 
@@ -22,15 +24,26 @@ class Game {
             height: canvas.height / Game.ZOOM_FACTOR
         };
         this._stars = new Array<Star>(Game.STAR_CNT);
+        this._loopHandle = null;
     }
 
-    public init(): void {
+    public start(): void {
+        if(this._loopHandle !== null) {
+            throw new Error("already started.");
+        }
+
         for (let i = 0; i < Game.STAR_CNT; i++) {
             this._stars[i] = new Star(this);
         }
+
+        this._loopHandle = setInterval(() => this.gameLoop(), Game.GAME_SPEED);
     }
 
-    public draw(): void {
+    private gameLoop(): void {
+        this.draw();
+    }
+
+    private draw(): void {
         this._ctx.fillStyle = "black";
         this._ctx.fillRect(0, 0, this.SCREEN_SIZE.width, this.SCREEN_SIZE.height);
         this._stars.forEach((star) => star.draw(this._ctx));

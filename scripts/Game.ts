c53b4248c63f766998c2e1ab39ec9d07a4322c19
jswolf19 <jswolf19@hotmail.com>
2021-06-05 19:46:44 +0900

    index.html, Game.ts追加

---
+++ scripts/Game.ts
@@ -0,0 +1,27 @@
+class Game {
+    private static readonly ZOOM_FACTOR: number = 2;
+
+    public readonly SCREEN_SIZE: Size;
+
+    public get FIELD_SIZE(): Size {
+        return {
+            width: this.SCREEN_SIZE.width * 2,
+            height: this.SCREEN_SIZE.height * 2
+        };
+    }
+    
+    private readonly _ctx: CanvasRenderingContext2D;
+
+    public constructor(canvas: HTMLCanvasElement) {
+        this._ctx = canvas.getContext("2d");
+        this.SCREEN_SIZE = {
+            width: canvas.width / Game.ZOOM_FACTOR,
+            height: canvas.height / Game.ZOOM_FACTOR
+        };
+    }
+}
+
+interface Size {
+    readonly width: number;
+    readonly height: number;
+}

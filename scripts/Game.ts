2c1d5aec05aa02378880314fabd780d77f320a25
jswolf19 <jswolf19@hotmail.com>
2021-06-08 23:06:33 +0900

    Rectangleを使用する

--- scripts/Game.ts
+++ scripts/Game.ts
@@ -3,14 +3,10 @@ class Game {
     private static readonly STAR_CNT = 300;
     private static readonly GAME_SPEED = 1000 / 60;
 
-    public get CAMERA_LOCATION(): ScaledPoint {
-        return new ScaledPoint(
-            ScaledNumber.from(0),
-            ScaledNumber.from(0)
-        )
+    public get screenBounds(): Rectangle {
+        return new Rectangle({x: 0, y: 0}, this._screenSize);
     }
-
-    public readonly SCREEN_SIZE: Size;
+    private readonly _screenSize: Size;
 
     public get FIELD_SIZE(): Size {
         return {
@@ -32,7 +28,7 @@ class Game {
         this._sprites = sprites;
 
         this._ctx = canvas.getContext("2d");
-        this.SCREEN_SIZE = {
+        this._screenSize = {
             width: canvas.width / Game.ZOOM_FACTOR,
             height: canvas.height / Game.ZOOM_FACTOR
         };
@@ -40,8 +36,8 @@ class Game {
         this._loopHandle = null;
 
         this._vcanvas = document.createElement("canvas");
-        this._vcanvas.width = this.SCREEN_SIZE.width * 2;
-        this._vcanvas.height = this.SCREEN_SIZE.height * 2;
+        this._vcanvas.width = this._screenSize.width * 2;
+        this._vcanvas.height = this._screenSize.height * 2;
         this._vctx = this._vcanvas.getContext("2d");
     }
 
@@ -63,28 +59,38 @@ class Game {
         }
     }
 
-    public isVisible(location: ScaledPoint, size: Size = { width: 0, height: 0 }): boolean {
-        return location.x < this.CAMERA_LOCATION.x + this.SCREEN_SIZE.width &&
-                location.x + size.width >= this.CAMERA_LOCATION.x && 
-                location.y < this.CAMERA_LOCATION.y + this.SCREEN_SIZE.height &&
-                location.y + size.height >= this.CAMERA_LOCATION.y; 
+    public isVisible(test: Point | Rectangle): boolean {
+        let screenBounds = this.screenBounds;
+        let testBounds: Rectangle = Game.isRectangle(test) ? test : new Rectangle(test, {width: 0, height: 0});
+
+        return testBounds.left < screenBounds.right &&
+                testBounds.right >= screenBounds.left && 
+                testBounds.top < screenBounds.bottom &&
+                testBounds.bottom >= screenBounds.top; 
+    }
+    private static isRectangle(obj: Point | Rectangle): obj is Rectangle {
+        return typeof (obj as Rectangle).width !== "undefined";
     }
 
     private gameLoop(): void {
+        let screenBounds = this.screenBounds;
+ 
         this._stars.forEach((star) => star.update(this));
 
         this.draw(this._vctx);
 
         this._ctx.drawImage(
             this._vcanvas, 
-            this.CAMERA_LOCATION.x, this.CAMERA_LOCATION.y, this.SCREEN_SIZE.width, this.SCREEN_SIZE.height,
+            screenBounds.left, screenBounds.top, screenBounds.width, screenBounds.height,
             0, 0, this._ctx.canvas.width, this._ctx.canvas.height
         );
     }
 
     private draw(ctx: CanvasRenderingContext2D): void {
+        let screenBounds = this.screenBounds;
+
         ctx.fillStyle = "black";
-        ctx.fillRect(0, 0, this.SCREEN_SIZE.width, this.SCREEN_SIZE.height);
+        ctx.fillRect(screenBounds.left, screenBounds.top, screenBounds.width, screenBounds.height);
         this._stars.filter((star) => star.isVisible(this))
                    .forEach((star) => star.draw(ctx));
     }

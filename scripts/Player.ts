82cfdbce71b97e5729f8209b5478aa8e74be46ac
jswolf19 <jswolf19@hotmail.com>
2021-06-13 19:11:05 +0900

    Playerを動かせる

--- scripts/Player.ts
+++ scripts/Player.ts
@@ -1,6 +1,12 @@
 class Player implements Drawable {
     private _location: ScaledPoint;
+    private _maxSpeed: ScaledNumber;
+
+    private get sprite(): Sprite {
+        return this._sprites[this._currentSpriteIdx];
+    }
     private readonly _sprites: Array<Sprite>;
+    private _currentSpriteIdx: number;
 
     public constructor(location: ScaledPoint, spriteLoader: SpriteLoader) {
         this._location = location;
@@ -9,6 +15,8 @@ class Player implements Drawable {
             spriteLoader.getSprite(Rectangle.from({x: 31, y: 0}, {x: 69, y: 35})),
             spriteLoader.getSprite(Rectangle.from({x: 71, y: 0}, {x: 99, y: 35})),
         ];
+        this._currentSpriteIdx = 1;
+        this._maxSpeed = ScaledNumber.from(2);
     }
 
     public isVisible(game: Game): boolean {
@@ -16,9 +24,23 @@ class Player implements Drawable {
     }
 
     public draw(ctx: CanvasRenderingContext2D): void {
-        this._sprites[1].draw(ctx, this._location);
+        this.sprite.draw(ctx, this._location);
     }
 
     public update(game: Game): void {
+        let newLocation: ScaledPoint = this._location;
+        if(game.keyboardInput.leftPressed) {
+            newLocation = newLocation.offset({x: this._maxSpeed.negate(), y: ScaledNumber.from(0)});
+        }
+        if(game.keyboardInput.upPressed) {
+            newLocation = newLocation.offset({x: ScaledNumber.from(0), y: this._maxSpeed.negate()});
+        }
+        if(game.keyboardInput.rightPressed) {
+            newLocation = newLocation.offset({x: this._maxSpeed, y: ScaledNumber.from(0)});
+        }
+        if(game.keyboardInput.downPressed) {
+            newLocation = newLocation.offset({x: ScaledNumber.from(0), y: this._maxSpeed});
+        }
+        this._location = newLocation;
     }
 }
